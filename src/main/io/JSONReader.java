86aed41741b0dd5e09bd664ecf62ee11c142e977
Bruna Ferreira <8230509@estg.ipp.pt>
2025-12-03 15:24:55 +0000

    Enigmas
    - Implementação da leitura do JSON de Enigmas
    - Implementação da classe Enigma (escolha dos enigmas)

--- src/main/io/JSONReader.java
+++ src/main/io/JSONReader.java
@@ -1,4 +1,50 @@
 package main.io;
 
+import com.google.gson.Gson;
+
+import java.io.File;
+import java.io.FileReader;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import main.data.impl.list.LinkedList;
+import main.model.Enigma;
+
 public class JSONReader {
+
+    private final String enigmaFilePath;
+
+    public JSONReader() {
+        this("resource-files/enigmas.json");
+    }
+
+    public JSONReader(String enigmaFilePath) {
+        this.enigmaFilePath = enigmaFilePath;
+    }
+
+    public LinkedList<Enigma> lerEnigmas() {
+        LinkedList<Enigma> enigmas = new LinkedList<>();
+
+        try (FileReader reader = new FileReader(enigmaFilePath)) {
+            Gson gson = new Gson();
+            Enigma[] arrayTemp = gson.fromJson(reader, Enigma[].class);
+
+            for (Enigma e : arrayTemp) {
+                if (e.getIdEnigma() == null || e.getIdEnigma().isEmpty() || e.getPergunta() == null || e.getPergunta().isEmpty() || e.getResposta() == null || e.getResposta().isEmpty()) {
+                    System.out.println("Enigma inválido encontrado: " + e);
+                    continue;
+                } else {
+                    enigmas.add(e);
+                }
+            }
+
+        } catch (JsonSyntaxException e) {
+            System.out.println("JSON mal formado: " + e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return enigmas;
+    }
 }
--- src/main/model/Enigma.java
+++ src/main/model/Enigma.java
@@ -1,4 +1,82 @@
 package main.model;
 
+import main.data.impl.list.LinkedList;
+import main.data.impl.queue.LinkedQueue;
+
 public class Enigma {
+
+    private String idEnigma;
+    private String pergunta;
+    private String resposta;
+
+    LinkedQueue<Enigma> poll = new LinkedQueue<>();
+    LinkedQueue<Enigma> disponiveis = new LinkedQueue<>();
+
+    public Enigma() {
+    }
+
+    public String getIdEnigma() {
+        return idEnigma;
+    }
+
+    public String getPergunta() {
+        return pergunta;
+    }
+
+    public String getResposta() {
+        return resposta;
+    }
+
+    public void inicializarFilas(LinkedList<Enigma> listaEnigmas) {
+        for (Enigma e : listaEnigmas) {
+            disponiveis.enqueue(e);
+        }
+    }
+
+    public Enigma getProximoEnigma() {
+        if (disponiveis.isEmpty()) {
+            reciclarEnigmas();
+        }
+        if(disponiveis.isEmpty()) {
+            return null;
+        }
+
+        int tamanho = disponiveis.size();
+        int indiceAleatorio = (int) (Math.random() * tamanho);
+
+
+        Enigma e = removerEnigmaPorIndice(disponiveis, indiceAleatorio);
+        poll.enqueue(e);
+
+        return e;
+    }
+
+    private Enigma removerEnigmaPorIndice(LinkedQueue<Enigma> disponiveis, int indiceAleatorio) {
+        LinkedQueue<Enigma> listaEnigma = new LinkedQueue<>();
+        Enigma enigmaRemovido = null;
+        int i = 0;
+
+        while (!disponiveis.isEmpty()) {
+            Enigma e = disponiveis.dequeue();
+            if (i == indiceAleatorio) {
+                enigmaRemovido = e;
+            } else {
+                listaEnigma.enqueue(e);
+            }
+            i++;
+        }
+
+        while (!listaEnigma.isEmpty()) {
+            disponiveis.enqueue(listaEnigma.dequeue()); // Restaura os enigmas restantes
+        }
+
+        return enigmaRemovido;
+    }
+
+    private void reciclarEnigmas() {
+        while (!poll.isEmpty()) {
+            disponiveis.enqueue(poll.dequeue()); // Move todos os enigmas de volta para a fila de disponíveis
+        }
+    }
+
 }
